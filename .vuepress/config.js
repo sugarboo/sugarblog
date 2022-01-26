fea6fc13f15e880cbfe67583a9ee9015b637905d
kennethbai <bbkh@vip.qq.com>
2022-01-26 11:33:22 +0800

    update config.js, config.js, .gitignore

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   "title": "sugarblog",
   "description": "",
   "dest": "public",
+  "base": '/sugarblog/',
   "head": [
     [
       "link",
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 
 # 进入生成的文件夹
 cd public
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sugarboo/sugarblog.git dev:master
+git push -f git@github.com:sugarboo/sugarblog.git master:gh-pages
 
 cd -
