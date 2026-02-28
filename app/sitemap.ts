f263ac0826e1850d88f615c19f4966b1d042bde2
sugarboo <kennethbaak@gmail.com>
2026-02-28 10:18:03 +0800

    feat: update sitemap generation for blog posts and static pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 import { getPostListData } from '@/api'
 import { siteConfig } from '@/lib/site-config'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPostListData()
 
